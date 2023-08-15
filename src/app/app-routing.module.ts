fb2ca3f63c341cca05809e76f376abb9e474d33d
carlapeloia <capeloia@hotmail.com>
2023-08-15 14:26:59 -0300

    Criação e estilização da página não encontrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 
 const routes: Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-{ path: "", redirectTo: "produtos", pathMatch: "full"}
+{ path: "", redirectTo: "produtos", pathMatch: "full"},
+{ path: "**", component: NaoEncontradaComponent}
 ];
 
 @NgModule({
