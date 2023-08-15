068b8179168c1eb49a29d5f71ba796a539bc49cf
carlapeloia <capeloia@hotmail.com>
2023-08-15 14:17:05 -0300

    Criação da página de produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+{ path: "", redirectTo: "produtos", pathMatch: "full"}
+];
 
 @NgModule({
   declarations: [],
